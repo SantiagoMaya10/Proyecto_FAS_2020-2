fd03fc8fc8801d7847bd21ef804d92f8ace836f1
Julian Rojas <julianrojasbet@gmail.com>
2020-10-29 18:17:08 -0500

    Add user login and signup

---
+++ hv_estudiantes_back/core/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from core.models import Usuario
+
+
+class SignUpForm(UserCreationForm):
+    documento = forms.CharField(max_length=15, )
+    nombre = forms.CharField(max_length=70)
+    email = forms.EmailField()
+    telefono = forms.CharField(max_length=13, required=False, help_text='Opcional')
+    direccion = forms.CharField(max_length=50, required=False, help_text='Opcional')
+    fecha_nacimiento = forms.DateField(required=False, help_text='Opcional')
+    sexo = forms.CharField(max_length=10, required=False, help_text='Opcional')
+    nacionalidad = forms.CharField(max_length=30, required=False, help_text='Opcional')
+    tipo = forms.CharField(max_length=15, required=False, help_text='Opcional')
+    categoria = forms.CharField(max_length=15, required=False, help_text='Opcional')
+
+    class Meta:
+        model = Usuario
+        fields = ('username', 'password1', 'password2', 'documento', 'nombre', 'email', 'telefono', 'direccion',
+                  'fecha_nacimiento', 'sexo', 'nacionalidad', 'tipo', 'categoria')
--- hv_estudiantes_back/hv_estudiantes_back/urls.py
+++ hv_estudiantes_back/hv_estudiantes_back/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from core.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name="login"),
+    path('signup/', signup, name="signup"),
+
 ]
